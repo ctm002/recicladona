e1c496ff54f216bd3eabd195a93bfb6ede5734b2
Carlos Tapia Molina <ctapia@transbank.cl>
2020-12-09 16:32:42 -0300

    init commit

---
+++ app/src/main/java/cl/vikost/jasisalioquecosasno/MainActivity.java
@@ -0,0 +1,26 @@
+package cl.vikost.jasisalioquecosasno;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ImageViewCompat;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+   ImageViewCompat _foto;
+   Button _boton ;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//          _foto= findViewById(R.id.);
+
+
+
+
+    }
+}

cd99cdf5c682504310a9d93680ab1adce1026b02
ctm002 <carlostapiam@gmail.com>
2020-12-10 18:10:22 -0300

    Informacion de Contacto mas Pro
    Detalle mas dinamico de la informacion de contacto

--- app/src/main/java/cl/vikost/negocio/ContactoActivity.java
+++ app/src/main/java/cl/vikost/negocio/ContactoActivity.java
@@ -8,11 +8,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.database.sqlite.SQLiteDatabase;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import data.MyDataHelper;
 
 public class ContactoActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +23,26 @@ public class ContactoActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        String[] values = new String[] {
+                "Recicladona Spa",
+                "Representante Legal: Victoria Tapia",
+                "Domicilio: Cerro Catedral #9523, Villa Comendador, Pudahuel, Santiago",
+                "Celular: 98689025X"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                values);
+
+        ListView listView = (ListView) findViewById(R.id.lst_datos_contacto );
+        listView.setAdapter(adapter);
+
+        String titleBar = "Contacto";
+        if (getActionBar() != null) {
+            getActionBar().setTitle(titleBar);
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        } else {
+            getSupportActionBar().setTitle(titleBar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
     }
 
